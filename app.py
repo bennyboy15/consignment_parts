68aeefa3e89341da9945d8e7c0b2f4efcf99e4c6
Benjamin Harvey <benjaminharvey2000@gmail.com>
2025-05-27 14:37:54 +0930

    Ability to register/login

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
+from dotenv import load_dotenv
 
 db = SQLAlchemy()
+load_dotenv()
 
 # FACTORY DESIGN PATTERN
 def create_app():
     app = Flask(__name__, template_folder='application/templates', static_folder='application/static', static_url_path='/')
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./consignment.db'
-
+    app.secret_key = os.getenv("SECRET_KEY")
     db.init_app(app)
 
     from routes import register_routes
--- routes.py
+++ routes.py
@@ -1,4 +1,8 @@
-from flask import redirect, render_template, request, url_for
+from flask import flash, redirect, render_template, request, url_for
+from models import User
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
 
 def register_routes(app, db):
     
@@ -6,11 +10,38 @@ def register_routes(app, db):
     def home():
         return redirect(url_for("login"))
 
-    @app.route("/login")
+    @app.route("/login", methods=['GET', 'POST'])
     def login():
+        if request.method == "POST":
+            input_username = request.form['username']
+            input_password = request.form['password']
+
+            user = User.query.filter_by(username=input_username).first()
+
+            if user and bcrypt.check_password_hash(user.password, input_password):
+                return "Successful login!"
+            else:
+                flash("Invalid username or password", "danger")
+
         return render_template('login.html')
     
-    @app.route("/register")
+    @app.route("/register", methods=['GET', 'POST'])
     def register():
+        
+        if request.method == "POST":
+            new_user = User(
+                username=request.form['username'], 
+                password=bcrypt.generate_password_hash(request.form['password']).decode('utf-8'), 
+                role="Software Developer", 
+                description="Makes the software!"
+                )
+            try:
+                db.session.add(new_user)
+                db.session.commit()
+                return redirect(url_for("login"))
+            except:
+                flash("Error when registering")
+                return redirect(url_for("register"))
+            
         return render_template('register.html')
     
